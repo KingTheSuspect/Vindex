3e23aa49d121f1ffbff72bae10e8157bcee31441
KingTheSuspect <demironurcem@hotmail.com>
2023-06-03 13:25:40 +0300

    .

--- Assets/Scripts/Menu/SceneLoader.cs
+++ Assets/Scripts/Menu/SceneLoader.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Play()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    }
+    public void Settings()
     {
-        
+
+    }
+    public void Exit()
+    {
+
     }
 }
