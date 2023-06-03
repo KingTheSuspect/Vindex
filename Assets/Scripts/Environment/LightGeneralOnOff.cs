061bb99d6a2eb2406e210898d739c47665f5f16e
KingTheSuspect <demironurcem@hotmail.com>
2023-06-03 21:24:41 +0300

    .

---
+++ Assets/Scripts/Environment/LightGeneralOnOff.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightGeneralOnOff : MonoBehaviour
+{
+    public Light2D[] lights;
+    public float minIntensity = 0f;      // Minimum ýþýk parlaklýðý
+    public float maxIntensity = 1f;      // Maksimum ýþýk parlaklýðý
+    public float flickerSpeed = 1f;      // Parlaklýk deðiþtirme hýzý
+    private float targetIntensity;       // Hedef ýþýk parlaklýðý
+
+    private void Update()
+    {
+        foreach (var item in lights)
+        {
+            // Mevcut ýþýk parlaklýðýný hedef ýþýk parlaklýðýna doðru deðiþtirme
+            item.intensity = Mathf.Lerp(item.intensity, targetIntensity, flickerSpeed * Time.deltaTime);
+
+            // Hedef ýþýk parlaklýðýný deðiþtirme
+            if (Mathf.Abs(item.intensity - targetIntensity) <= 0.05f)
+            {
+                // Eðer mevcut hedef parlaklýk maksimum deðerdeyse, minimum deðere geçiþ yap
+                // Aksi halde, maksimum deðere geçiþ yap
+                targetIntensity = targetIntensity == maxIntensity ? minIntensity : maxIntensity;
+            }
+        }
+
+    }
+}
